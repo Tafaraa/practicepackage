4a6f4bda454ef66602de527a26e47eeed4b936b7
Your Name <you@example.com>
2024-02-28 20:48:13 +0200

    commit(package)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     description='EDSA example python package',
     long_description=open('README.md').read(),
     install_requires=['numpy'],
-    url='https://github.com/<username>/<package-name>',
-    author='<Your Name>',
-    author_email='<Your Email>'
+    url='https://github.com/Tafaraa/practicepackage.git',
+    author='<T>',
+    author_email='<mutsvedutafara@gmail.com>'
 )
